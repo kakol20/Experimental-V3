e20ffa35b3f430d76e5e63fc68ba70f78c6bb4ac
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2016-09-08 21:22:19 +0100

    Added Morse Code converter

--- main.js
+++ main.js
@@ -785,6 +785,88 @@ var averages = function() {
     document.getElementById('avgOutput').innerHTML = "Mode: " + mode(list) + ". Median: " + median(list) + ". Mean: " + mean(list);
 };
 
+var morseConvert = function() {
+    var morseCode = (function() {
+        return {
+            code: ["/", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "-.--.-", "-.--.-", "-....-", "--..--", ".-.-.-", "-..-.", "..--..", ".----."],
+            letters: " ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890()-,./?'",
+
+            fromSentence: function(a) {
+                a = a.toUpperCase();
+                var b = a.split("");
+                var c = [];
+                for (var i = 0; i < b.length; i++) {                        
+                    if ((morseCode.letters.indexOf(b[i]) <= -1) && (morseCode.letters.indexOf(b[i - 1]) > 0) && (morseCode.letters.indexOf(b[i + 1]) > 0)) {
+                        c.push(" ");
+                    } else if (morseCode.letters.indexOf(b[i]) > -1) {
+                        c.push(b[i]);
+                    }
+                }
+                var d = "";
+                for (var i = 0; i < c.length; i++) {
+                    var e = morseCode.code[morseCode.letters.indexOf(c[i])];
+                    if ((i + 1) == c.length) {
+                        d = d + e;
+                    } else {
+                        d = d + e + " ";
+                    }
+                }
+                return d;
+            },
+
+            toSentence: function(a) {
+                var b = a.split(" ");
+                var c = [];
+                for (var i = 0; i < b.length; i++) {
+                    if (morseCode.code.indexOf(b[i]) > -1) {
+                        c.push(b[i]);
+                    }
+                }
+                var d = "";
+                for (var i = 0; i < c.length; i++) {
+                    var e = morseCode.letters.charAt(morseCode.code.indexOf(c[i]));
+                    var d = d + e;
+                }
+                return d;
+            },
+
+            test: function(a) {
+                a = a || "Hello World";
+                console.log("Sentence: " + a);
+                var b = morseCode.fromSentence(a);
+                console.log("Sentence to Morse Code: " + b);
+                var c = morseCode.toSentence(b);
+                console.log("Morse Code to Sentence: " + c);
+                if (c !== a.toUpperCase()) {
+                    console.log("Test Failed");
+                }
+                console.log(" ");
+            }
+        };
+    })();
+
+    var input = document.getElementById('morseInput').value || "empty";
+
+    if (input == "empty") {
+        document.getElementById('morseOutput').innerHTML = "You must input something";
+    } else {
+        var isSentence = function(a) {
+            var b = a.split(" ");
+            for (var i = 0; i < b.length; i++) {
+                if (morseCode.code.indexOf(b[i]) <= -1) {
+                    return true;
+                }
+                return false;
+            }
+        };
+        if (isSentence(input)) {
+            document.getElementById('morseOutput').innerHTML = morseCode.fromSentence(input);
+        } else {
+            document.getElementById('morseOutput').innerHTML = morseCode.toSentence(input)
+        }
+    }
+};
+
 /*
 TODO List - 
 1. Enhancement {
